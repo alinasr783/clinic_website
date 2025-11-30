0b8bdb1c11dc55e32ebe6eb3a10705894f83bcaf
alinasr783 <alinasreldin783@gmail.com>
2025-11-30 15:22:48 +0200

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <Link to="/" className="flex flex-col items-center gap-1">
       <img
-        src="https://i.ibb.co/KzFqYBWY/Picture1.png"
+        src="https://i.ibb.co/pvSbXJHy/Gemini-Generated-Image-aokz26aokz26aokz-1.png"
         alt="Logo"
         className="w-8 h-8 sm:w-12 sm:h-12 rounded-full object-cover"
       />
